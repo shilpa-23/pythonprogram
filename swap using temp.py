24b67bdcad9857ff0d927835da09a07235f1840c
shilpa-23 <76903830+shilpa-23@users.noreply.github.com>
2021-06-28 13:34:02 +0530

    Add files via upload

---
+++ swap using temp.py
@@ -0,0 +1,16 @@
+x=input("Enter the first number:")
+y=input("Enter the second number:")
+print("Before swapping")
+print("x=")
+print(x)
+print("y=")
+print(y)
+temp=x
+x=y
+y=temp
+print("After swapping")
+print("x=")
+print(int(x))
+print("y=")
+print(int(y))
+
